f31dfb72adccfeea843e1ff839cddcbf59a73ec0
Paul Kim <iam@paulkim.me>
2024-10-16 00:24:30 -0400

    Fix Globe

--- scripts/globe.js
+++ scripts/globe.js
@@ -5,9 +5,10 @@ document.getElementById('globe-container').appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / 400, 0.1, 1000);
-camera.position.z = 400;
+camera.position.z = 200;
 
-const globe = new ThreeGlobe();
+const globe = new ThreeGlobe()
+    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg');
 scene.add(globe);
 
 const light = new THREE.AmbientLight(0xbbbbbb);
@@ -22,4 +23,11 @@ function animate() {
     globe.rotation.y += 0.005;
     renderer.render(scene, camera);
 }
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / 400;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, 400);
+});
+
 animate();
